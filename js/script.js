f727eec1589bb95e1353c2e3c73a78f1253175bf
shaowei-v <shaoweiw@berkeley.edu>
2026-06-10 12:44:29 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken =
 
 const myMap = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/shaoweiw/cmq7472d3001101sqhlj42u35/draft',
+  style: 'mapbox://styles/shaoweiw/cmq7472d3001101sqhlj42u35',
   center: [-122.27, 37.8],
   zoom: 9
 });
